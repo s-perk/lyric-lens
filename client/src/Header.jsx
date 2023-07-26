8963d3bd6a3ba3c4071ac6926133463d5059b623
Stephen Perkins <stperk21@gmail.com>
2023-07-26 11:15:22 -0400

    Switch dash server to auto-refresh, and simplify callbacks and components

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -4,32 +4,32 @@ import axios from 'axios'
 
 
 
-function Header({handleSubmit, handleChange, formData, setFormData}) {
+function Header({ handleSubmit, handleChange, formData, setFormData }) {
 
   return (
     <div className="flex h-max navbar bg-gray-800 sticky top-0 z-50 drop-shadow-[0_5px_3px_rgba(0,0,0,0.25)]">
-      <div className='flex basis-1/2 p-1'>
+      <div className='flex p-1 basis-1/2'>
         <a target="_blank" >
-          <img src={searchLogo} className="logo h-12 w-12" alt="Vite logo" />
+          <img src={searchLogo} className="w-12 h-12 logo" alt="Vite logo" />
         </a>
-        <h1 className='text-slate-100 self-center'>lyric lens</h1>
+        <h1 className='self-center text-slate-100'>lyric lens</h1>
       </div>
       <form id='song-search' className='flex justify-self-end' onSubmit={handleSubmit}>
-        <div  className='self-center pr-4 text-slate-100 items-center'>
+        <div className='items-center self-center pr-4 text-slate-100'>
           <label htmlFor='artist'>Artist: </label>
           <input type='text' name='artist' id='artist' value={formData.artist} onChange={handleChange} className='rounded p-[0.2rem]'></input>
         </div>
-        <div  className='self-center pr-4'>
+        <div className='self-center pr-4'>
           <label htmlFor='song' className='text-slate-100'>Song: </label>
           <input type='text' name='song' id='song' value={formData.song} onChange={handleChange} className='rounded p-[0.2rem]'></input>
         </div>
         {/* <div className=""> */}
-          <button
-            type='submit'
+        <button
+          type='submit'
 
-            className='button text-white rounded h-[2rem] self-center border-2 border-transparent bg-[#1a1a1a] cursor-pointer transition-colors p-1 hover:border-[#646cff] hover:border-2 focus:outline-4'>
-              Search
-          </button>
+          className='button text-white rounded h-[2rem] self-center border-2 border-transparent bg-[#1a1a1a] cursor-pointer transition-colors p-1 hover:border-[#646cff] hover:border-2 focus:outline-4'>
+          Search
+        </button>
         {/* </div> */}
 
       </form>
--- python/plotly_dash_server.py
+++ python/plotly_dash_server.py
@@ -1,11 +1,11 @@
-from dash import Dash, dcc, html, Input, Output
-import plotly.express as px
-import pandas as pd
 import time
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-import plotly_chart
 
+import pandas as pd
+import plotly.express as px
+import plotly_chart
+from dash import Dash, Input, Output, ctx, dcc, html
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
 colorscales = px.colors.named_colorscales()
 
@@ -17,109 +17,8 @@ scale = 'viridis'
 
 app = Dash(__name__)
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
+colors = {'background': '#111111', 'text': '#7FDBFF'}
 
-def refresh_data():
-    df = pd.read_pickle(pickle_filepath)
-    # df = px.data.iris() # replace with your own data source
-    # Need to figure out how to get the dataframe into this file at runtime
-        # A snapshot of this plot is served when it's started, so need to find a way to treat our dataframe as state, and not constant as being used here...
-
-        # Check this out:
-        # https://dash.plotly.com/sharing-data-between-callbacks
-    fig = px.scatter(
-        df,
-        x='x',
-        y='y',
-        size='count',
-        color='count',
-        color_continuous_scale=scale, #these are the colors of the dot. All available colors: https://plotly.com/python/builtin-colorscales/
-        hover_name='word',
-        size_max=15, #max size of the dots
-        width=1000,
-        height=1000,
-        range_color = [0, 42], #Adjust the end of the range for brighter colors
-        render_mode='svg'
-    )
-
-    fig.update_yaxes(scaleanchor = "x", scaleratio = 1, showgrid=False,title='',visible=False)
-    fig.update_xaxes(showgrid=False,title='',visible=False)
-    fig.update_layout(template='plotly_dark') #template options: "plotly", "plotly_white", "plotly_dark", "ggplot2", "seaborn", "simple_white", "none"
-
-    # df = px.data.iris() # replace with your own data source
-    # fig = px.scatter(
-    #     df,
-    #     # x="sepal_width",
-    #     # y="sepal_length",
-    #     # color="sepal_length",
-    #     # color_continuous_scale=scale,
-    # )
-    return fig
-
-
-def setup_app():
-
-
-    app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-        # html.H4('Interactive Plotly Express color scale selection'),
-        # html.P("Color Scale"),
-        html.Div(id='time-stamp', children=[time.time()], style={'backgroundColor': colors['background'], 'color': 'aquamarine'}),
-        html.Div(id='your-component-id', style={'backgroundColor': colors['background'], 'color': 'aquamarine'}),
-
-        dcc.Dropdown(
-            id='builtin-colorscales-x-dropdown',
-            options=colorscales,
-            value='viridis'
-        ),
-
-        html.Center(
-            dcc.Graph(id="builtin-colorscales-x-graph")
-        )
-
-    ])
-
-    # Callback to update data set
-    @app.callback(
-        Output('your-component-id', 'children'),
-        # Output("builtin-colorscales-x-graph", "figure"),
-        Input('time-stamp', 'children')
-    )
-    def update_file(change):
-        update_data()
-        print('updating dataframe at ' + time.strftime("%H:%M:%S"))
-        # Your logic to update the visualization components with the new data
-        return html.Div("Data refreshed at: " + time.strftime("%H:%M:%S"))
-
-
-
-    # Callback to automatically refresh graph based on colorscales
-    @app.callback(
-        Output("builtin-colorscales-x-graph", "figure"),
-        Input("builtin-colorscales-x-dropdown", "value")
-    )
-    def change_colorscale(sc):
-        global scale
-        scale = sc
-        return refresh_data()
-
-
-    if __name__ == "__main__":
-        app.run_server(debug=True)
-
-
-
-# Set up event handler to update graph every time .pkl file updates
-class FileChangeHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        print('pickle file changed!')
-        print('event.is_directory', event.is_directory)
-        print('event.src_path', event.src_path)
-        if not event.is_directory and event.src_path.endswith('.pkl'):
-            print('run event handler!')
-            update_data()
 
 # Function to update data
 def update_data():
@@ -127,11 +26,102 @@ def update_data():
     plotly_data = pd.read_pickle(pickle_filepath)
 
 
+def refresh_data():
+    df = pd.read_pickle(pickle_filepath)
+    # df = px.data.iris() # replace with your own data source
+    # Need to figure out how to get the dataframe into this file at runtime
+    # A snapshot of this plot is served when it's started, so need to find a way to treat our dataframe as state, and not constant as being used here...
+
+    # Check this out:
+    # https://dash.plotly.com/sharing-data-between-callbacks
+    fig = px.scatter(
+        df,
+        x='x',
+        y='y',
+        size='count',
+        color='count',
+        color_continuous_scale=scale,  # these are the colors of the dot. All available colors: https://plotly.com/python/builtin-colorscales/
+        hover_name='word',
+        size_max=15,  # max size of the dots
+        width=1000,
+        height=1000,
+        range_color=[0, 42],  # Adjust the end of the range for brighter colors
+        render_mode='svg',
+    )
+
+    fig.update_yaxes(
+        scaleanchor="x", scaleratio=1, showgrid=False, title='', visible=False
+    )
+    fig.update_xaxes(showgrid=False, title='', visible=False)
+    fig.update_layout(
+        template='plotly_dark'
+    )  # template options: "plotly", "plotly_white", "plotly_dark", "ggplot2", "seaborn", "simple_white", "none"
+
+    return fig
+
+
+def setup_app():
+    app.layout = html.Div(
+        style={'backgroundColor': colors['background']},
+        children=[
+            html.Div(
+                id='time-stamp-formatted',
+                style={'backgroundColor': colors['background'], 'color': 'aquamarine'},
+            ),
+            dcc.Dropdown(
+                id='colorscales-dropdown',
+                options=colorscales,
+                value='viridis',
+            ),
+            dcc.Interval(
+                id='interval-component',
+                interval=10 * 1000,  # in milliseconds
+                n_intervals=0,
+            ),
+            html.Center(dcc.Graph(id="pretty-graph")),
+        ],
+    )
+
+    # Callback to automatically refresh graph based on colorscales and time stamp
+    @app.callback(
+        Output("pretty-graph", "figure"),
+        Output("time-stamp-formatted", "children"),
+        Input("colorscales-dropdown", "value"),
+        Input("interval-component", "n_intervals"),
+    )
+    def update_graph(sc, interval):
+        triggered_id = ctx.triggered_id
+        timestamp = html.Div("Data refreshed at: " + time.strftime("%H:%M:%S %p"))
+
+        if triggered_id == 'colorscales-dropdown':
+            global scale
+            scale = sc
+            return refresh_data(), timestamp
+
+        else:
+            return refresh_data(), timestamp
+
+    if __name__ == "__main__":
+        app.run_server(debug=True)
+
+
+# Set up event handler to update graph every time .pkl file updates
+class FileChangeHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if not event.is_directory and event.src_path.endswith('.pkl'):
+            print('pickle file changed!')
+            print('run event handler!')
+            update_data()
+
+
 # Set up event handlers
 observer = Observer()
-observer.schedule(FileChangeHandler(), path='/Users/StephenPerkins/Documents/Coding/Hack Reactor/MVP/Lyric Lens/python/', recursive=False)
+observer.schedule(
+    FileChangeHandler(),
+    path='/Users/StephenPerkins/Documents/Coding/Hack Reactor/MVP/Lyric Lens/python/',
+    recursive=False,
+)
 observer.start()
 
 
 setup_app()
-
